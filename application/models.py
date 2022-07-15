a7e18a1fa063673ce6cc2e29fe8538d94801277a
deepsn458 <deepaknarayan458@gmail.com>
2022-07-15 23:14:12 +0800

    quiz structure

--- application/models.py
+++ application/models.py
@@ -1,6 +1,43 @@
 from application import db, login_manager
 from flask_login import UserMixin
 
+# Quiz questions table
+class Questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_name = db.Column(db.Text, nullable=False)
+    displayed = db.Column(db.Boolean, default=False)
+    #to link each option to its question
+    options = db.relationship('Options', backref='questions', lazy=True)
+    # to retrieve question from responses table
+    response = db.relationship('Responses', backref='questions', lazy=True)
+    def __repr__(self):
+        return f"Questions('{self.question_name}')"
+
+
+#Quiz options table
+class Options(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    option_name = db.Column(db.String(20), nullable=False)
+    correct_incorrect = db.Column(db.Boolean, default=False)
+    #to link each option to its question
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'),
+                            nullable=False)
+    #to retrieve the selected option from responses table
+    response = db.relationship('Responses', backref='options', lazy=True)
+    def __repr__(self):
+        return f"Options('{self.option_name}')"
+
+# Quiz responses table
+class Responses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'),
+                            nullable=False)
+    option_id = db.Column(db.Integer, db.ForeignKey('options.id'),
+                            nullable=False)
+    #takes whether the chosen option was correct or not from the options table
+    correct_incorrect= db.Column(db.Boolean,default=False)
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -14,9 +51,3 @@ class User(db.Model, UserMixin):
     # Represents the User table's objects as strings
     def __repr__(self):
         return f"User('{self.username}')"
-
-# User's summary for quiz
-
-# User's total statistics
-
-# Quiz database
