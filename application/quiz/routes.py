8f9ee4e311cf9b0f3a50fa73c87163bc37e1576f
deepsn458 <deepaknarayan458@gmail.com>
2022-07-27 16:45:53 +0800

    JS Done

--- application/quiz/routes.py
+++ application/quiz/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, request, redirect, jsonify
+from flask import Blueprint, render_template, url_for, request, redirect, jsonify, make_response
 from application.models import Questions, Options, Responses
 from application import db
 import random
@@ -6,53 +6,87 @@ from sqlalchemy.sql import func
 
 Quiz = Blueprint('Quiz', __name__)
 
-# actual quiz page
+# actual quiz page 
 @Quiz.route("/quiz", methods=["GET", "POST"])
 def quiz():
+    # if page is accessed from start page, return the quiz page
+    if request.method == "GET":
+        return render_template("quiz.html")
+
+    # if timer runs out, return the results page
+    else:
+        return redirect(url_for('Quiz.results'))
+
+
+# Asynchronous queries for questions and options after button is clicked
+@Quiz.route("/query", methods=["GET", "POST"])
+def query():
+    
+    #sends question to quiz page and retrieves the option
+    if request.method == "POST":
+        response = request.get_json()
+        print(response['answer'])
+        print(response['question'])
+        
+        #updates the 'Responses' table with the question and option
+        q = Questions.query.filter_by(question_name=response['question']).first()
+        o = Options.query.filter_by(option_name=response['answer']).first()
+        resp = Responses(question_id=q.id, option_id=o.id, correct_incorrect=o.correct_incorrect)
+        db.session.add(resp)
+        db.session.commit()
+
     # Retrieve a question at random from the 'Questions' table
     index = random.randint(1,20)
-    q = Questions.query.filter_by(id=index, displayed=0).first()
-
+    q = Questions.query.filter_by(id=index).first()
     q.displayed = 1
     db.session.commit()
 
     # Selects the correct option for the question
     o1 = Options.query.filter_by(question_id=index, correct_incorrect=True).first()
-
     # Randomely selects an incorrect option for the questiom
     o2 =  Options.query.filter_by(question_id=index, correct_incorrect=False).order_by(func.random()).limit(1).first()
+    # random number to decide which button correct answer should be on
     btn_id = random.randint(0,1)
+    req = {
+        "question": q.question_name,
+        "correct": o1.option_name,
+        "wrong":o2.option_name,
+        "id":btn_id
+    }
+    response = make_response(jsonify(req), 200)
+    return response
+    
 
-    # Updates 'Responses' table when option is chosen
-    if request.method == "POST":
-        option = Options.query.filter_by(id=request.form.get("response")).first()
-        response = Responses(question_id=q.id, option_id=option.id, correct_incorrect=option.correct_incorrect)
-        db.session.add(response)
-        db.session.commit()
 
-        # return the results page when timer runs out
-        return redirect(url_for('Quiz.results'))
-    else:
-        return render_template("quiz.html", btn_id=btn_id, o1=o1, o2=o2, q=q)
 
+   
 @Quiz.route("/quiz/results")
 def results():
     # displays the number of correct and incorrect answers
     total_correct = Responses.query.filter_by(correct_incorrect=True).count()
     total_incorrect = Responses.query.filter_by(correct_incorrect=False).count()
-    # Clears the responses table for the next quiz
-    db.session.query(Responses).delete()
-    db.session.commit()
 
-
-     # sets the displayed values for all the questions back to false
-    for i in range(1,21):
-        question = Questions.query.get(i)
-        question.displayed = 0
-        db.session.commit()
     return render_template("results.html", total_correct=total_correct, total_incorrect=total_incorrect)
-
 
 @Quiz.route("/quiz/summary")
 def summary():
-    return render_template("summary.html")
+    #Table:
+    #C1: Index
+    #C2: Question
+    #C3: Your response
+    #C4: Current Response
+
+    #Stores 'Responses' table in a list
+    table = []
+
+    #Each row in 'Responses' is stored in a dictionary
+    row_count = Responses.query.count()
+    for i in range(1,row_count):
+        response = Responses.query.get(i)
+        row = {}
+        row['index'] = i
+        row['question'] = Questions.query.get(response.question_id).question_name
+        row['user_answer'] = Options.query.get(response.option_id).option_name
+        row['correct_answer'] = Options.query.filter_by(question_id=response.question_id, correct_incorrect=True).first().option_name
+        table.append(row)
+    return render_template("summary.html",table=table)
--- application/static/quiz.js
+++ application/static/quiz.js
@@ -1,18 +1,3 @@
-/*
-When quiz page is loaded 3 second timer starts
-
-    A POST request is sent to obtai the question, options, and btn_id
-    If btn_id == 0:
-        correct option is assigned to left button
-    Else
-        correct option is assigned to right button
-    60 second timer is started 
-    When submit button is pressed 
-        A POST request is sent containing the name and value of the option
-        Another request for a question,options, and btn_id will be sent
-*/
-
-
 $(document).ready(function() {
     //sets default value of timers
     localStorage.setItem('60_second', 60);
@@ -21,7 +6,11 @@ $(document).ready(function() {
     shortTimer();
     displayQuestions();
 
-    
+    //When the button is clicked, Function to send the response to database and retrieve a new question is called
+   $("button").click(function(){
+    sendResponse(this.id);
+    return false;
+   })  
 })
 
 //60 second timer
@@ -31,6 +20,8 @@ function shortTimer() {
         //stops timer after 60 seconds and sends a request to redirect to the results page
         if (time == 0) {
             clearInterval(timer_id);
+            $("#timer").html("Time is Up!")
+            finishQuiz();
             return;
         }
         //updates timer
@@ -40,8 +31,95 @@ function shortTimer() {
     }, 1000);
 }
 
-//Sends a request 
+//Sends a request to retrieve a question
 function displayQuestions() {
-   console.log('ye');
 
+   //sends request to quiz route
+   fetch(`${window.origin}/query`)
+ 
+    .then(function(response){
+        if (response.status !== 200) {
+            console.log(`Response status was not 200: ${response.status}`);
+            return;
+        }
+
+        response.json().then(function (data) {
+            //gets the question name 
+            $("#question").html(data['question']);
+
+            if (data['id'] == 0) {
+                // Assigns the correct option to right button if the random number is 0
+                $("#right").val(data['correct']);
+                $("#right").html(data['correct']);
+
+                $("#left").val(data['wrong']);
+                $("#left").html(data['wrong']);
+            }
+            else {
+                // Assigns the correct option to left button if the random number is 1
+                $("#right").val(data['wrong']);
+                $("#right").html(data['wrong']);
+                $("#left").val(data['correct']);
+                $("#left").html(data['correct']);
+            }
+        })
+    })
+}
+
+// sends the user's response to the responses database and gets back a new question
+function sendResponse(id) {
+    console.log($(`#${id}`).val());
+    fetch(`${window.origin}/query`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        //sends the question and the user's answer to the database
+        body: JSON.stringify({
+            question: $("#question").html(),
+            answer: $(`#${id}`).val()
+        })
+    })
+
+    .then(function(response){
+        if(response.status !== 200) {
+            console.log(`Response status was not 200: ${response.status}`);
+            return;
+        }
+
+        response.json()
+        .then(function(data){
+            //gets the question name 
+            $("#question").html(data['question']);
+
+            if (data['id'] == 0) {
+                // Assigns the correct option to right button if the random number is 0
+                $("#right").val(data['correct']);
+                $("#right").html(data['correct']);
+
+                $("#left").val(data['wrong']);
+                $("#left").html(data['wrong']);
+            }
+            else {
+                // Assigns the correct option to left button if the random number is 1
+                $("#right").val(data['wrong']);
+                $("#right").html(data['wrong']);
+                $("#left").val(data['correct']);
+                $("#left").html(data['correct']);
+            }
+        })
+    })
+}
+
+//Sends request to redirect to results page after timer runs out
+function finishQuiz() {
+    fetch(`${window.origin}/quiz/results`)
+    .then(function(response){
+        if(response.status !== 200) {
+            console.log(`Response status was not 200: ${response.status}`);
+            return;
+        }
+        //redirects to the results route
+        window.location = response.url;
+    })
 }
